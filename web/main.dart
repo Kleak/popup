6643ec5f870177c2616c27d32998be1b85d72638
Kleak <segaud.kevin@gmail.com>
2017-09-01 10:21:19 +0200

    first commit

---
+++ web/main.dart
@@ -0,0 +1,10 @@
+// Copyright (c) 2017, kleak. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular/angular.dart';
+
+import 'package:popup_test/app_component.dart';
+
+void main() {
+  bootstrap(AppComponent);
+}
